000f93879a9023b2cb9f1bb293ce41e486403a14
Duc Thang <thangnd@psnet.asia>
2022-08-03 13:54:38 +0700

    https://jena.apache.org/documentation/inference/#owl

--- src/main/java/sema_jena/Read4_7.java
+++ src/main/java/sema_jena/Read4_7.java
@@ -3,37 +3,40 @@ package sema_jena;
 import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.reasoner.Reasoner;
 import org.apache.jena.reasoner.ReasonerRegistry;
 import org.apache.jena.riot.RDFDataMgr;
-
+import org.apache.jena.util.PrintUtil;
 
 // https://www.programcreek.com/java-api-examples/?class=org.apache.jena.riot.RDFDataMgr&method=loadModel
 public class Read4_7 {
 
 	public static void main(String[] args) {
-		Model schema = RDFDataMgr.loadModel("assets/practice/4.7-schema.rdf");
-		Model data = RDFDataMgr.loadModel("assets/practice/4.7-data.ttl");
+		// https://jena.apache.org/documentation/inference/#owl
+		owlExample();
+	}
+	
+	static void owlExample() {
+		Model schema = RDFDataMgr.loadModel("assets/practice/owlDemoSchema.rdf");
+		Model data = RDFDataMgr.loadModel("assets/practice/owlDemoData.rdf");
 		Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
-		reasoner.bindSchema(schema);
-		InfModel infModel =  ModelFactory.createInfModel(reasoner, data);
-		infModel.getResource("http://www.hust.edu.vn/thangnd#");
-		
-		final StmtIterator iter = infModel.listStatements();
-	    while (iter.hasNext()) {
-	    	Statement stmt = iter.nextStatement();
+		reasoner = reasoner.bindSchema(schema);
 
-	        String sub = stmt.getSubject().toString();
-	    	String pred = stmt.getPredicate().toString();
-	        String obj = stmt.getObject().toString();
-	        
-	        System.out.println(sub);
-	        System.out.println(pred);
-	        System.out.println(obj);
-	    }
-		
+		InfModel infmodel = ModelFactory.createInfModel(reasoner, data);
+		Resource nForce = infmodel.getResource("urn:x-hp:eg/nForce");
+		System.out.println("nForce *:");
+		printStatements(infmodel, nForce, null, null);
+	}
+
+	public static void printStatements(Model m, Resource s, Property p, Resource o) {
+		for (StmtIterator i = m.listStatements(s, p, o); i.hasNext();) {
+			Statement stmt = i.nextStatement();
+			System.out.println(" - " + PrintUtil.print(stmt));
+		}
 	}
 
 }
